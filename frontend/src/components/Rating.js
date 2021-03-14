e04a4b9a831c7331bd73f25e16384eb62691b31c
Shiva Gupta <shivagupta@gmail.com>
2021-03-14 22:37:31 +0530

    added rating and products

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+const Rating = ({ value, text, color }) => {
+  const rating = [];
+  for (let i = 0; i < 5; i++) {
+    if (value >= 1 + i) {
+      rating.push(<i className="fas fa-star" style={{ color }}></i>);
+    } else if (value >= i + 0.5) {
+      rating.push(<i className="fas fa-star-half-alt" style={{ color }}></i>);
+    } else {
+      rating.push(<i className="far fa-star" style={{ color }}></i>);
+    }
+  }
+  return (
+    <div className="rating">
+      <span className="stars">{rating}</span>
+      <span>{text && text}</span>
+    </div>
+  );
+};
+
+Rating.defaultProps = {
+  color: "#f8e825",
+};
+Rating.prototype = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+};
+export default Rating;
